066a1ac20bca48759cab02c7ee9fe9d80f70425b
marmstr3 <brokenfighter@hotmail.com>
2019-12-26 12:20:02 -0700

    Added test for SudokuCoordinate.Value = 10. Added test for SudokuCoordinate.Value = ‘a’ Added test for SudokuCoordinate.Value = 1.1 Added test for SudokuCoordinate.Value = SudokuCoordinate()

--- tests/test_SudokuCoordinate.py
+++ tests/test_SudokuCoordinate.py
@@ -34,7 +34,7 @@ class InitTestCase(unittest.TestCase):
         Tests for when the value of the coordinate is the expected maximum, 9
         """
         value = SudokuCoordinate(9, (0,0)).value
-        self.assertEqal(9, value)
+        self.assertEqual(9, value)
         
     def test_value_is_negative(self):
         """
@@ -43,5 +43,39 @@ class InitTestCase(unittest.TestCase):
         """
         with self.assertRaises(ValueError):
             SudokuCoordinate(-1, (0,0))
+         
+    def test_value_is_10(self):
+        """
+        Tests for when the value of the coordinate is an unexpected large 
+        integer, 10
+        """
+        with self.assertRaises(ValueError):
+            SudokuCoordinate(10, (0,0))
+            
+    def test_value_is_a(self):
+        """
+        Tests for when the value of the coordinate is an unexpected character,
+        'a'
+        """
+        with self.assertRaises(ValueError):
+            SudokuCoordinate('a', (0,0))
+            
+    def test_value_is_one_point_one(self):
+        """
+        Tests for when the value of the coordinate is an unexpected float,
+        1.1
+        """
+        with self.assertRaises(ValueError):
+            SudokuCoordinate(1.1, (0.0))
         
+    def test_value_is_coordinate_object(self):
+        """
+        Tests for when the value of the coordinate is an unexpected object,
+        SudokuCoordinate
+        """
+        with self.assertRaises(ValueError):
+            SudokuCoordinate(SudokuCoordinate(0,(0,0)), (0,0))
+            
+    
+    
 unittest.main()
